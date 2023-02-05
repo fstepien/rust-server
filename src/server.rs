023876a261d7c6336c72c176f91239127db1d06a
Filip Stepien <filip.stepien@gmail.com>
2023-02-05 13:57:10 -0500

    lifetimes

---
+++ src/server.rs
@@ -0,0 +1,37 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::io::Read;
+use std::net::TcpListener;
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    pub fn run(self) {
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        println!("Listening on {}", self.addr);
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Connection failed {}", e),
+            }
+        }
+    }
+}
